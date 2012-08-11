408672fd511cda01604f30bb64eb82c1e318043e
Rik Blok <rik.blok@ubc.ca>
2012-08-10 22:05:39 -0700

    Coding and testing, stage 2.

--- syntax/applet.php
+++ syntax/applet.php
@@ -49,26 +49,30 @@ class syntax_plugin_netlogo_applet extends DokuWiki_Syntax_Plugin {
 //    }
 
     public function handle($match, $state, $pos, &$handler){
-        $data = array();
+        $match=substr($match,10,-2); // strip leading "{{netlogo>" and trailing "}}"
+		preg_match( '/[^ /', $match, $match_file);
 		/*
-    preg_match( '/width=([0-9]+)/i',            substr( $match, 6, -1 ), $match_width   );
-    preg_match( '/height=([0-9]+)/i',           substr( $match, 6, -1 ), $match_height  );
-    preg_match( '/code=([a-zA-Z_0-9].+)/i',      substr( $match, 6, -1 ), $match_code    );
-    preg_match( '/archive=([a-zA-Z_0-9:.]+)/i', substr( $match, 6, -1 ), $match_archive );
-    return array( 
-      $match_code[1], 
-      $match_width[1], 
-      $match_height[1], 
-      $match_archive[1] 
-    );
-	*/
-
-        return $data;
+		preg_match( '/width=([0-9]+)/i',            substr( $match, 6, -1 ), $match_width   );
+		preg_match( '/height=([0-9]+)/i',           substr( $match, 6, -1 ), $match_height  );
+		preg_match( '/code=([a-zA-Z_0-9].+)/i',      substr( $match, 6, -1 ), $match_code    );
+		preg_match( '/archive=([a-zA-Z_0-9:.]+)/i', substr( $match, 6, -1 ), $match_archive );
+		*/
+		return array( 
+			$match_file[1]
+		);
     }
 
     public function render($mode, &$renderer, $data) {
         if($mode != 'xhtml') return false;
-		$renderer->doc .= "<p>NetLogo placeholder</p>";   
+		list( $file ) = $data;
+		$renderer->doc .= "[applet code=\"org.nlogo.lite.Applet\""
+								. "        archive=\"netlogolite/5.0.1/NetLogoLite.jar\""
+								. "        width=\"844\" height=\"690\">"
+								. "  [param name=\"DefaultModel\""
+								. "        value=\"".$file."\">"
+								. "  [param name=\"java_arguments\""
+								. "        value=\"-Djnlp.packEnabled=true\">"
+								. "[/applet>";
         return true;
     }
 }
