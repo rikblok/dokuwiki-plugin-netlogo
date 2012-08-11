34e321113b7975c42d8b5c50bc25ff922245cf72
Rik Blok <rik.blok@ubc.ca>
2012-08-10 21:03:14 -0700

    Coding in progress. Testing.

--- syntax/applet.php
+++ syntax/applet.php
@@ -4,6 +4,10 @@
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
  * @author  Rik Blok <rik.blok@ubc.ca>
+ * 
+ * Acknowledgements:
+ * Thanks to Stylianos Dritsas for the applet plugin 
+ *   <https://www.dokuwiki.org/plugin:applet>.
  */
 
 // must be run within Dokuwiki
@@ -17,20 +21,26 @@ require_once DOKU_PLUGIN.'syntax.php';
 
 class syntax_plugin_netlogo_applet extends DokuWiki_Syntax_Plugin {
     public function getType() {
-        return 'FIXME: container|baseonly|formatting|substition|protected|disabled|paragraphs';
+        return 'substition';
     }
 
+/* // don't override getPType()
     public function getPType() {
         return 'FIXME: normal|block|stack';
     }
+*/
 
     public function getSort() {
-        return FIXME;
+		/* Should be less than 320 as defined in
+		 * /inc/parser/parser.php:class Doku_Parser_Mode_media
+		 * http://xref.dokuwiki.org/reference/dokuwiki/_classes/doku_parser_mode_media.html
+		*/
+        return 317; // after plugin:applet
     }
 
 
     public function connectTo($mode) {
-        $this->Lexer->addSpecialPattern('<FIXME>',$mode,'plugin_netlogo_applet');
+        $this->Lexer->addSpecialPattern('\{\{netlogo>[^\}]+\}\}',$mode,'plugin_netlogo_applet');
 //        $this->Lexer->addEntryPattern('<FIXME>',$mode,'plugin_netlogo_applet');
     }
 
@@ -40,13 +50,25 @@ class syntax_plugin_netlogo_applet extends DokuWiki_Syntax_Plugin {
 
     public function handle($match, $state, $pos, &$handler){
         $data = array();
+		/*
+    preg_match( '/width=([0-9]+)/i',            substr( $match, 6, -1 ), $match_width   );
+    preg_match( '/height=([0-9]+)/i',           substr( $match, 6, -1 ), $match_height  );
+    preg_match( '/code=([a-zA-Z_0-9].+)/i',      substr( $match, 6, -1 ), $match_code    );
+    preg_match( '/archive=([a-zA-Z_0-9:.]+)/i', substr( $match, 6, -1 ), $match_archive );
+    return array( 
+      $match_code[1], 
+      $match_width[1], 
+      $match_height[1], 
+      $match_archive[1] 
+    );
+	*/
 
         return $data;
     }
 
     public function render($mode, &$renderer, $data) {
         if($mode != 'xhtml') return false;
-
+		$renderer->doc .= "<p>NetLogo placeholder</p>";   
         return true;
     }
 }
