158db7e018d88c89ed645232fa07c8dd3ae88f7f
Rik Blok <rik.blok@ubc.ca>
2012-09-21 11:18:46 -0700

    Try 20

--- syntax/servefile.php
+++ syntax/servefile.php
@@ -9,12 +9,6 @@
 // must be run within Dokuwiki
 if (!defined('DOKU_INC')) die();
 
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-
-require_once DOKU_PLUGIN.'syntax.php';
-
 // can't access file directly because of DokuWiki folder permissions so server it up thru php
 $src = DOKU_INC.'data/media/playground/test.nlogo';
 echo file_get_contents($src);
