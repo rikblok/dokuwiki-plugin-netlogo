b081ca436bd89a879af12d037e1844b542281c0e
Rik Blok <rik.blok@ubc.ca>
2012-09-23 11:12:20 -0700

    Try 28

--- libraries/5.0.1/servefile.php
+++ libraries/5.0.1/servefile.php
@@ -6,6 +6,7 @@
  * @author  Rik Blok <rik.blok@ubc.ca>
  */
 
+/* Debugging: remove path [Rik, 2012-09-23]
 // must be run within Dokuwiki
 if (!defined('DOKU_INC')) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
@@ -13,6 +14,9 @@ if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 // can't access file directly because of DokuWiki folder permissions so serve it up thru php
 // debugging: $src should be passed from applet.php [Rik, 2012-09-21]
 $src = DOKU_PLUGIN.'netlogo/libraries/5.0.1/test.nlogo';
+*/
+$src = 'test.nlogo';
+
 echo file_get_contents($src);
 //$renderer->doc .= '<code>'.file_get_contents($src).'</code>';
 // vim:ts=4:sw=4:et:
