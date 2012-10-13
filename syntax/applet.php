dbe3c6ce705466c40e57a4d0192dcfd1839ae73b
Rik Blok <rik.blok@ubc.ca>
2012-10-12 20:59:15 -0700

    Debug 10

--- syntax/applet.php
+++ syntax/applet.php
@@ -52,7 +52,7 @@ class syntax_plugin_netlogo_applet extends DokuWiki_Syntax_Plugin {
 
     public function connectTo($mode) {
 //        $this->Lexer->addSpecialPattern('\{\{netlogo>[^\}]+\}\}',$mode,'plugin_netlogo_applet');	// replaced, look for {{*.nlogo}} instead of {{netlogo>*}} [Rik, 2012-10-12]
-        $this->Lexer->addSpecialPattern('[\{]{2}netlogo> ?[^\{\} ]+[\.]nlogo(\?[^{} ]+)? ?[\}]{2}',$mode,'plugin_netlogo_applet');	// replaced, look for {{*.nlogo}} instead of {{netlogo>*}}.  Help from http://www.pagecolumn.com/tool/pregtest.htm [Rik, 2012-10-12]
+        $this->Lexer->addSpecialPattern('\{\{[^\}\{]*?\.nlogo(\?.*)? ?\}\}',$mode,'plugin_netlogo_applet');	// replaced, look for {{*.nlogo}} instead of {{netlogo>*}}.  Help from http://www.pagecolumn.com/tool/pregtest.htm [Rik, 2012-10-12]
 //        $this->Lexer->addSpecialPattern('\{\{netlogo>[^\}]+\.nlogo(\?[^\}]+)?\s?\}\}',$mode,'plugin_netlogo_applet');	// look for {{*.nlogo}} instead of {{netlogo>*}}.  Todo: How to download file instead of running? Debugging: doesn't work. [Rik, 2012-10-12]
 //        $this->Lexer->addEntryPattern('<FIXME>',$mode,'plugin_netlogo_applet');
     }
@@ -67,7 +67,7 @@ class syntax_plugin_netlogo_applet extends DokuWiki_Syntax_Plugin {
 		 * http://xref.dokuwiki.org/reference/dokuwiki/_functions/doku_handler_parse_media.html
 		*/
 		// Strip the opening and closing markup
-		$link = preg_replace(array('/^\{\{/netlogo>','/\}\}$/u'),'',$match);	
+		$link = preg_replace(array('/^\{\{/','/\}\}$/u'),'',$match);	
 		
 		// Split title from URL
 		$link = explode('|',$link,2);
