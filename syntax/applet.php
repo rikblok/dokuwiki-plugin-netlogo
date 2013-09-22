be3c1df5b70bbb4cde476aed39133c4c1134a12a
Rik Blok <rik.blok@ubc.ca>
2013-09-21 21:38:54 -0700

    Revert "testing codebase for includes 9"
    This reverts commit e0b7a1db7f4f1ef7d8ae113d9e94e6e2991a02d9.

--- syntax/applet.php
+++ syntax/applet.php
@@ -310,8 +310,7 @@ class syntax_plugin_netlogo_applet extends DokuWiki_Syntax_Plugin {
 			revision 5 - can't find jar
 			revision 6 - can't find jar
 			revision 7 - incompatible magic value.  I think it found the jar but not the nlogo file
-			revision 8 - correction basename includes extension
-			revision 9 - ??
+			revision 8 - ??
 		*/
 		
 		if ($pcenter) $renderer->doc .= '<p align="center">';
@@ -323,7 +322,7 @@ class syntax_plugin_netlogo_applet extends DokuWiki_Syntax_Plugin {
 		if (!is_null($data['title']))	$renderer->doc .= ' alt="'.$data['title'].'"';
 		$renderer->doc .= '>'
 								. '  <param name="DefaultModel"'
-								. '      value="'.basename($src).'">'
+								. '      value="'.basename($src).'.nlogo">'
 //								. '      value="lib/plugins/netlogo/inc/servefile.php?src='.urlencode($src).'&expires='.$expires.'&token='.urlencode($token).'">'
 								. '  <param name="java_arguments"'
 								. '      value="-Djnlp.packEnabled=true">'
